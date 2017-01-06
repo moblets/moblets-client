1d4f65e8e2f1513b5444fc2de88561e8df705a0d
Rodrigo Real <rodrigoreal@live.com>
2017-01-06 11:20:35 -0200

    feat(database): add crud methods to manipulate the data

--- src/database/database.js
+++ src/database/database.js
@@ -1,14 +1,122 @@
 const request = require('request');
+const LocalStorage = require('../utils/localstorage');
 
 module.exports = class Database {
   constructor(config) {
     this.config = config;
+    switch (config.environment) {
+      case 'dev':
+        this.url = 'http://daia.universo2.local:8282';
+        break;
+      case 'production':
+        this.url = 'http://daia.universo2.local:8282';
+        break;
+      default:
+        this.url = 'http://localhost:8282';
+    }
   }
 
-  get() {
+  get(path) {
     return new Promise((resolve, reject) => {
-      request('url', (error, response, body) => {
-        resolve(body);
+      const url = `${this.url}/app/${this.config.app}/instances/${path}`;
+      request(url, (error, response, body) => {
+        let bodyParsed;
+        try {
+          bodyParsed = JSON.parse(body);
+        } catch (e) {
+          reject(body);
+        }
+        if (error) {
+          reject(error);
+        } else if (response.statusCode !== 200) {
+          reject(bodyParsed);
+        } else {
+          resolve(bodyParsed);
+        }
+      });
+    });
+  }
+
+  push(path, data = {}) {
+    return new Promise((resolve, reject) => {
+      const options = {
+        url: `${this.url}/app/${this.config.app}/instances/${path}`,
+        form: {
+          data: JSON.stringify(data),
+        },
+        headers: {
+          Authorization: `Bearer ${LocalStorage.get(`fabapp:authUser:${this.config.app}`).tokenManager.accessToken}`,
+        },
+      };
+      request.post(options, (error, response, body) => {
+        let bodyParsed;
+        try {
+          bodyParsed = JSON.parse(body);
+        } catch (e) {
+          reject(body);
+        }
+        if (error) {
+          reject(error);
+        } else if (response.statusCode !== 201) {
+          reject(bodyParsed);
+        } else {
+          resolve(bodyParsed);
+        }
+      });
+    });
+  }
+
+  update(path, data = {}) {
+    return new Promise((resolve, reject) => {
+      const options = {
+        url: `${this.url}/app/${this.config.app}/instances/${path}`,
+        form: {
+          data: JSON.stringify(data),
+        },
+        headers: {
+          Authorization: `Bearer ${LocalStorage.get(`fabapp:authUser:${this.config.app}`).tokenManager.accessToken}`,
+        },
+      };
+      request.put(options, (error, response, body) => {
+        let bodyParsed;
+        try {
+          bodyParsed = JSON.parse(body);
+        } catch (e) {
+          reject(body);
+        }
+        if (error) {
+          reject(error);
+        } else if (response.statusCode !== 200) {
+          reject(bodyParsed);
+        } else {
+          resolve(bodyParsed);
+        }
+      });
+    });
+  }
+
+  remove(path) {
+    return new Promise((resolve, reject) => {
+      const options = {
+        url: `${this.url}/app/${this.config.app}/instances/${path}`,
+        headers: {
+          Authorization: `Bearer ${LocalStorage.get(`fabapp:authUser:${this.config.app}`).tokenManager.accessToken}`,
+        },
+      };
+      request.delete(options, (error, response, body) => {
+        let bodyParsed;
+        try {
+          bodyParsed = JSON.parse(body);
+        } catch (e) {
+          reject(body);
+        }
+        if (error) {
+          reject(error);
+        } else if (response.statusCode !== 204) {
+          reject(bodyParsed);
+        } else {
+          resolve();
+        }
       });
     });
   }
