872519ca8a8f43e1f158e944a08fe2314bba9154
Rodrigo Real <rodrigoreal@live.com>
2017-01-09 13:29:22 -0200

    refactor(project): change the project name from Fabrica to MobletsClient

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ const Database = require('./database/database');
 const Authentication = require('./authentication/authentication');
 const User = require('./user/user');
 
-const Fabrica = class Fabrica {
+const MobletsClient = class MobletsClient {
   constructor(config) {
-    this.config = config;
     this.user = new User(config);
     this.database = new Database(config);
     this.auth = new Authentication(config);
   }
 };
 
-module.exports = Fabrica;
+module.exports = MobletsClient;
